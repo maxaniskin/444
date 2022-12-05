588bd0754a8e9f05e57e2a0bbad5a6e30ce09530
maxaniskin <109670058+maxaniskin@users.noreply.github.com>
2022-12-06 00:53:53 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+var x = document.getElementById("containt");
+
+x.addEventListener("click", myFunction);
+
+function myFunction() {
+  var element = document.getElementById("nav1");
+  element.classList.toggle("open");
+x.addEventListener("click", myFunction);
+  
+}
+
